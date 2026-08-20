ae5c3f9fa130d60bd0fff85a7b1bd96a78cf6e05
0800 <spmspy0800@gmail.com>
2026-08-20 18:08:35 +0200

    Enhance blocksi removal functionality
    Refactor blocksi removal logic to use MutationObserver for dynamic detection.

--- blocksi_remover.js
+++ blocksi_remover.js
@@ -1,13 +1,30 @@
-
-  const blocksiUrl = "chrome-extension://ghlpmldmjjhmdgmneoaibbegkjjbonbk/images/icons/yt-denied.png";
+const blocksiUrl = "chrome-extension://ghlpmldmjjhmdgmneoaibbegkjjbonbk/images/icons/yt-denied.png";
 
 fetch(blocksiUrl)
     .then(response => {
         if (response.ok) {
-          console.log("blocksi extension detected");
-            setTimeout(() => {
+            console.log("blocksi extension detected");
+
+            function removeBlocksiElement() {
                 document.querySelector("#__blocksi_iframe_blocked__")?.remove();
+            }
+
+            setTimeout(() => {
+                if (!document.querySelector("#__blocksi_iframe_blocked__") && window.self !== window.top) {
+                    setTimeout(removeBlocksiElement, 2000);
+                } else {
+                    removeBlocksiElement();
+                }
             }, 2000);
+
+            new MutationObserver(() => {
+                if (document.querySelector("#__blocksi_iframe_blocked__")) {
+                    removeBlocksiElement();
+                }
+            }).observe(document.documentElement, {
+                childList: true,
+                subtree: true
+            });
         }
     })
     .catch(() => {});
